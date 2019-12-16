79fa389f5a8d5173440b903cc6d6ebfe31fb046c
Tony Dantona <tonydantona@gmail.com>
2019-12-16 09:07:26 -0500

    Add project files.

---
+++ ECommerce.ProductCatalog/ServiceFabricProductRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ECommerce.ProductCatalog.Model;
+using Microsoft.ServiceFabric.Data;
+using Microsoft.ServiceFabric.Data.Collections;
+
+namespace ECommerce.ProductCatalog
+{
+    class ServiceFabricProductRepository : IProductRepository
+    {
+        // in order to talk to storage we need a reference to IReliableStateManager
+        // i.e. a built in SF interface to work with storage
+        private readonly IReliableStateManager _stateManager;
+
+        public ServiceFabricProductRepository(IReliableStateManager stateManager)
+        {
+            _stateManager = stateManager;
+        }
+
+        public async Task AddProduct(Product product)
+        {
+            // get me a reference to a dictionary or if it doesn't exist create one and give me the reference
+            IReliableDictionary<Guid, Product> products = await _stateManager
+            .GetOrAddAsync<IReliableDictionary<Guid, Product>>("products");
+
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                await products
+                   .AddOrUpdateAsync(tx, product.Id, product, (id, value) => product);
+
+                await tx.CommitAsync();
+            }
+        }
+
+        public async Task<IEnumerable<Product>> GetAllProducts()
+        {
+            IReliableDictionary<Guid, Product> products =
+            await _stateManager.GetOrAddAsync<IReliableDictionary<Guid, Product>>("products");
+            var result = new List<Product>();
+
+            using (ITransaction tx = _stateManager.CreateTransaction())
+            {
+                Microsoft.ServiceFabric.Data.IAsyncEnumerable<KeyValuePair<Guid, Product>> allProducts =
+                   await products.CreateEnumerableAsync(tx, EnumerationMode.Unordered);
+
+                using (Microsoft.ServiceFabric.Data.IAsyncEnumerator<KeyValuePair<Guid, Product>> enumerator =
+                   allProducts.GetAsyncEnumerator())
+                {
+                    while (await enumerator.MoveNextAsync(CancellationToken.None))
+                    {
+                        KeyValuePair<Guid, Product> current = enumerator.Current;
+                        result.Add(current.Value);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
